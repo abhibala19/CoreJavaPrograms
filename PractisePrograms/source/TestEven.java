6a5f6c8309977aa917940ebe63aaeeef441435a8
abhibala19 <abhinaya.2595@gmail.com>
2021-07-21 20:38:53 +0530

    Two programs added

---
+++ PractisePrograms/source/TestEven.java
@@ -0,0 +1,30 @@
+public class TestEven
+{
+    public static void main(String[] args)
+    {
+        repeatNoOfTimes(8);
+    }
+    public static void repeatNoOfTimes(int num)
+    {
+        if(isEven(num))
+        {
+            print("shambo", num);
+        }
+        else
+        {
+           print("govinda", num);
+        }
+    }
+
+    public static void print(String key, int num) {
+        for(int i=0;i<=num;i++)
+            {
+                System.out.println(key);
+            }
+    }
+
+    public static boolean isEven(int num){
+        return (num&1)==0;
+    }
+
+}
