c24146d4eeb3676107ed737b21a3605828758cba
abhibala19 <abhinaya.2595@gmail.com>
2021-07-23 15:16:28 +0530

    prime,gcf and lcm added

---
+++ PractisePrograms/source/Symbol.java
@@ -0,0 +1,17 @@
+public class Symbol
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=65535;i++)
+        {
+            if(i%5!=0)
+            {
+            System.out.print((char)i );
+            System.out.print((" " );
+            }
+            else
+               continue;
+   
+        }
+    }
+}
